3a76571ab9df98bf130e1e16af4d3f0af3cc5ad9
DRGO <abokado0901@gmail.com>
2024-03-03 05:22:10 +0900

    [Add]BlockPlayer、AttackStateの形式の変更(Monobehavior継承など)、ChageAttackStateの追加。

--- Assets/Scripts/RPGMode/BlockPlayerState/AttackStateBlock.cs
+++ Assets/Scripts/RPGMode/BlockPlayerState/AttackStateBlock.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 [Serializable]
-public class AttackStateBlock : IRPGState
+public class AttackStateBlock : MonoBehaviour,IRPGState
 {
+    SkillInfomation _normalSkill;
     float _time;
     [SerializeField] float _attackCoolTime = 2f;
+    bool _isAttackTime;
 
     public void Init(BlockPlayerController player)
     {
+        // BlockSkillの通常攻撃スキルを_skillに代入する。
+        var set = player._dataBase.BlockSkillSelectData.SkillInfomation[player._dataBase._blockSkillSetNo[0]];
+        _normalSkill = set;
+        _isAttackTime = false;
         _time = 0;
     }
 
     public void StartState(BlockPlayerController player)
     {
+        _isAttackTime = false;
         Debug.Log("攻撃開始:Block");
     }
 
@@ -30,30 +38,64 @@ public class AttackStateBlock : IRPGState
             //player.OnChangeState(player.RightCounterActiveTime);
         }
 
-        _time += Time.deltaTime;
+        if (!_isAttackTime)
+        {
+            _time += Time.deltaTime;
+        }
+
         if (_time > _attackCoolTime)
         {
             //チャージが100以上になったら状態をチャージアタックのStateに移行する。
             if (player._guageAttack >= 100)
             {
                 // チャージアタックに移行
-                //stateMachine.OnChangeState(stateMachine.ChageAttack);
+                player.OnChangeState(player.ChageAttackState);
             }
             else
             {
-                Attack(player);
+                _isAttackTime = true;
+                StartCoroutine(AttackTime(player));
             }
         }
     }
 
     /// <summary>スキルで敵にダメージを与えるメソッドa</summary>
     /// <param name="player"></param>
+    IEnumerator AttackTime(BlockPlayerController player)
+    {
+        player.ShowText(_normalSkill._skillName);
+        player._guageAttack += 1;
+        ///Animationつける
+        
+        ///
+        //アニメーション処理実装の際、アニメーションの秒数を代入
+        float animationTime = 0f;
+        for(var i = 0;i < animationTime;i++)
+        {
+            if(Input.GetButtonDown("BlockLeft") || Input.GetButtonDown("BlockRight"))
+            {
+                ///アニメーション中断処理
+
+                ///
+                Debug.Log("攻撃中断。");
+                yield break;
+            }   //ボタン入力されたらアニメーション中断してコルーチンを抜ける。
+            yield return null;
+        }
+
+        // アニメーションのフラグから処理を呼ぶ可能性もあるが今の所ここに攻撃処理を書く。
+        Attack(player);
+
+        _isAttackTime = false;
+
+        yield return null;
+    }
+
+    /// <summary>攻撃処理</summary>
+    /// <param name="player"></param>
     void Attack(BlockPlayerController player)
     {
-        var set = player._dataBase.BlockSkillSelectData.SkillInfomation[player._dataBase._blockSkillSetNo[0]];
-        player.ShowText(set._skillName);
-        player._guageAttack += 1;
-        if (set._selectSkill is BlockSkillSelect blockSkill)
+        if (_normalSkill._selectSkill is BlockSkillSelect blockSkill)
         {
             player._enemy.AddDebuffDamage(
                 player.Attack,
---
+++ Assets/Scripts/RPGMode/BlockPlayerState/ChageAttackState.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChageAttackState : MonoBehaviour,IRPGState
+{
+    SkillInfomation _chageSkill;
+    public void Init(BlockPlayerController player)
+    {
+        var set = player._dataBase.BlockSkillSelectData.SkillInfomation[player._dataBase._blockSkillSetNo[1]];
+        _chageSkill = set;
+    }
+
+    public void StartState(BlockPlayerController player)
+    {
+        player.ShowText("チャージアタック開始");
+        StartCoroutine(ChangeAttackTime(player));
+    }
+
+    public void UpdateState(BlockPlayerController player)
+    {
+        return;
+    }
+
+    public void EndState(BlockPlayerController player)
+    {
+        player.ShowText("チャージアタック終了");
+    }
+
+    /// <summary>チャージアタックのアニメーション処理</summary>
+    /// <param name="player"></param>
+    /// <returns></returns>
+    IEnumerator ChangeAttackTime(BlockPlayerController player)
+    {
+        //チャージアタックをした後、ゲージを０にして、Attack状態に戻る。
+        Debug.Log(_chageSkill._skillName);
+        ///Animationプレイ
+
+        ///
+        //アニメーション処理実装の際、アニメーションの秒数を代入
+        float animationTime = 0f;
+        //チャージアタックアニメーション中、時間を止めるならまた形式を変える。
+        yield return new WaitForSeconds(animationTime);
+
+        ChageAttack(player);
+        player.OnChangeState(player.AttackState);
+        yield return null;
+    }
+
+    /// <summary>チャージアタック</summary>
+    /// <param name="player"></param>
+    void ChageAttack(BlockPlayerController player)
+    {
+        if (_chageSkill._selectSkill is BlockSkillSelect blockSkill)
+        {
+            player._enemy.AddDebuffDamage(player.Attack, blockSkill.AttackValue, blockSkill.EnemyOffencePower, blockSkill.EnemyDiffencePower);
+        }
+    }
+
+}
